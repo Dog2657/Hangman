ad9a4bc66b800754bcb637a196c2a0d2f0b5addb
Dog2657 <reeceya2006@gmail.com>
2023-12-16 00:07:01 +0000

    Updated Loader to be more relitive

--- client/src/components/Loader.svelte
+++ client/src/components/Loader.svelte
@@ -1,37 +1,45 @@
-<i/>
+<section class="loader">
+    <i />
+</section>
 
 <style lang="scss">
-    i{
-        color: #ffffff8e;
-        font-size: 7px;
+    section.loader{
         position: relative;
-        text-indent: -9999em;
-        transform: translateY(-25%) translateZ(0);
-        animation-delay: -0.16s;
+        height: 17.5px;
+        text-align: center;
+
+        & > i{
+            color: #ffffff8e;
+            font-size: 7px;
+            position: relative;
+            text-indent: -9999em;
+            transform: translateY(-100%) translateX(-50%) translateZ(0);
+            animation-delay: -0.16s;
 
 
-        &, &::before, &::after{
-            border-radius: 50%;
-            width: 2.5em;
-            height: 2.5em;
-            animation-fill-mode: both;
-            animation: bblFadInOut 1.8s infinite ease-in-out;
-            content: '';
-            position: absolute;
-            top: 0;
-        }
+            &, &::before, &::after{
+                border-radius: 50%;
+                width: 2.5em;
+                height: 2.5em;
+                animation-fill-mode: both;
+                animation: bblFadInOut 1.8s infinite ease-in-out;
+                content: '';
+                position: absolute;
+                top: 0;
+            }
 
-        &::before{
-            left: -3.5em;
-            animation-delay: -0.32s;
-        }
+            &::before{
+                left: -3.5em;
+                animation-delay: -0.32s;
+            }
 
-        &::after{
-            left: 3.5em;
-            animation-delay: 0.32s;
+            &::after{
+                left: 3.5em;
+                animation-delay: 0.32s;
+            }
         }
     }
-
+    
     @keyframes bblFadInOut {
         0%, 80%, 100% { box-shadow: 0 2.5em 0 -1.3em }
         40% { box-shadow: 0 2.5em 0 0 }
